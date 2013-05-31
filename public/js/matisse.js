0dfe4c77e0442feff293153dca09826c9d4e6312
Jed Wood <jed@limechile.com>
2013-05-31 00:55:38 -0500

    possibly slayed the 300ms demons\!

--- public/js/matisse.js
+++ public/js/matisse.js
@@ -1,19 +1,19 @@
-var fastButtons = {
+// var fastButtons = {
 
-  replace: function() {
-    // copy the current click events on document
-    var clickEvents = jQuery.hasData( document ) && jQuery._data( document ).events.click;
-    clickEvents = jQuery.extend(true, {}, clickEvents);
+//   replace: function() {
+//     // copy the current click events on document
+//     var clickEvents = jQuery.hasData( document ) && jQuery._data( document ).events.click;
+//     clickEvents = jQuery.extend(true, {}, clickEvents);
 
-    // remove these click events
-    $(document).off('click');
+//     // remove these click events
+//     $(document).off('click');
 
-    // reset them as vclick events
-    for (var i in clickEvents) {
-      $(document).on('vclick', clickEvents[i].handler);
-    }
-  }
-};
+//     // reset them as vclick events
+//     for (var i in clickEvents) {
+//       $(document).on('vclick', clickEvents[i].handler);
+//     }
+//   }
+// };
 
 $(document).bind('pageinit', function(){
 
@@ -36,22 +36,23 @@ $(document).bind('pageinit', function(){
 });
 
 $(document).ready(function() {
-  fastButtons.replace();
+  //fastButtons.replace();
+  FastClick.attach(document.body);
 });
 
-$('body').on('tap', '.ui-panel-dismiss', function(event) {
-  console.log("ui-panel-dismiss TAP");
-  $sm = $('#sidemenu');
-  if ($sm.hasClass('ui-panel-open')) {
-    $sm.panel('close');
-  }
-  event.preventDefault();
-});
+// $('body').on('tap', '.ui-panel-dismiss', function(event) {
+//   console.log("ui-panel-dismiss TAP");
+//   $sm = $('#sidemenu');
+//   if ($sm.hasClass('ui-panel-open')) {
+//     $sm.panel('close');
+//   }
+//   event.preventDefault();
+// });
 
-$('body').on('tap', '.side-menu-btn', function(event) {
-  console.log("side-menu-btn TAP");
-  $sm = $('#sidemenu');
-  $sm.panel('open');
-  $('.ui-panel-content-wrap-closed').addClass('ui-panel-content-wrap-open').removeClass('.ui-panel-content-wrap-closed');
-  event.preventDefault();
-});
+// $('body').on('tap', '.side-menu-btn', function(event) {
+//   console.log("side-menu-btn TAP");
+//   $sm = $('#sidemenu');
+//   $sm.panel('open');
+//   $('.ui-panel-content-wrap-closed').addClass('ui-panel-content-wrap-open').removeClass('.ui-panel-content-wrap-closed');
+//   event.preventDefault();
+// });
